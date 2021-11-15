d06835a7188ccbc26cba9ecf3dfe470adb0a4650
Codetest Deployrepeat <codetestdeployrepeat@gmail.com>
2021-11-15 06:16:04 +0000

    chore: add faker to simplify fake image generator

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var faker = require('faker');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('OK');
+  const url = faker.image.image();
+  const data = {
+    url
+  };
+  res.send(data);
+});
+
+router.post('/', function(req, res, next) {
+  const url = faker.image.image();
+  const data = {
+    url
+  };
+  res.send(data);
 });
 
 module.exports = router;
